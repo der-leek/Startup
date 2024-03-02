ff5f18228a048ed8be4c1651523118f42447a187
Derek Ashby <dbashby@byu.edu>
2024-03-01 21:39:11 -0700

    Added typewriter function

--- whispertxt/index.js
+++ whispertxt/index.js
@@ -1,3 +1,20 @@
+function typewriter(element, text, index=18) {
+    if (index < text.length) {
+        element.innerHTML = text.slice(0, index);
+        index++;
+        
+        let random_delay = Math.random() * 10 + 50; // Base delay
+        if (Math.random() < 0.25) { // 25% chance of a pause
+            random_delay += Math.random() * 300; // Add up to 0.3 seconds extra
+        }
+
+        setTimeout(() => typewriter(element, text, index), random_delay);
+    } else {
+        element.innerHTML = text.slice(0, index);
+        setTimeout(() => window.location.href = "upload.html", 500)
+    }
+}
+
 function login() {
     const user_id = document.querySelector("#username");
     localStorage.setItem("user_name", user_id.value);
@@ -5,9 +22,8 @@ function login() {
 
     let welcome = document.getElementById("welcome");
     
-    text = welcome.textContent + ", " + user_id.value;
-    typewriter(welcome, text);
     if (user_id.value != "" && password.value != "") {
-        // window.location.href = "upload.html";
+        text = welcome.textContent + ", " + user_id.value;
+        typewriter(welcome, text);
     }
   }
