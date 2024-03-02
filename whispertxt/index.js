322e715f8107ed47d305bafa65435f47cdfa75ee
Derek Ashby <dbashby@byu.edu>
2024-03-01 21:35:17 -0700

    Added basic login functionality

--- whispertxt/index.js
+++ whispertxt/index.js
@@ -1,3 +1,13 @@
 function login() {
-    console.log("log me in!");
-}
+    const user_id = document.querySelector("#username");
+    localStorage.setItem("user_name", user_id.value);
+    const password = document.querySelector("#password");
+
+    let welcome = document.getElementById("welcome");
+    
+    text = welcome.textContent + ", " + user_id.value;
+    typewriter(welcome, text);
+    if (user_id.value != "" && password.value != "") {
+        // window.location.href = "upload.html";
+    }
+  }
