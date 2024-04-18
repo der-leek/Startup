01a755919c63c063b936ecca9fa5eb565fe4014d
Derek Ashby <dbashby@byu.edu>
2024-04-17 21:35:11 -0600

    configured for deployment

--- Service/transcribe.sh
+++ Service/transcribe.sh
@@ -1,2 +1,2 @@
-./whisper.cpp/main -m /Users/derekashby/Library/CloudStorage/OneDrive-BrighamYoungUniversity/ML/Winter2024/CS260/Startup/Service/whisper.cpp/models/ggml-base.bin -f ./uploads/output.wav -otxt -of ./downloads/output
-# ./whisper.cpp/main -m /home/ubuntu/services/startup/whisper.cpp/models/ggml-base.bin -f ./uploads/output.wav -otxt -of ./downloads/output
+# ./whisper.cpp/main -m /Users/derekashby/Library/CloudStorage/OneDrive-BrighamYoungUniversity/ML/Winter2024/CS260/Startup/Service/whisper.cpp/models/ggml-base.bin -f ./uploads/output.wav -otxt -of ./downloads/output
+./whisper.cpp/main -m /home/ubuntu/services/startup/whisper.cpp/models/ggml-base.bin -f ./uploads/output.wav -otxt -of ./downloads/output
